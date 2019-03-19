acadc39638e0d521191c87fdc163998b72eeed38
lisavdpasch <47452821+lisavdpasch@users.noreply.github.com>
2019-03-19 15:15:08 +0100

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,7 @@
+## Plot 2
+plot(t$Global_active_power~t$dateTime, type="l", 
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+## Save file and close device
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
